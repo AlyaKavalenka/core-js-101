7bd619d52d39edf6a8e6ad1ca4eb830c92b79bc3
Alya Kovalenko <kovalenkoalinam@gmail.com>
2022-12-27 08:00:12 +0300

    feat: solve some promises tasks

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -28,8 +28,11 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+function willYouMarryMe(isPositiveAnswer) {
+  const str = (isPositiveAnswer === true) ? 'Hooray!!! She said "Yes"!' : 'Oh no, she said "No".';
+
+  return (isPositiveAnswer !== undefined) ? Promise.resolve(str)
+    : Promise.reject(new Error('Wrong parameter is passed! Ask her again.'));
 }
 
 
@@ -48,8 +51,8 @@ function willYouMarryMe(/* isPositiveAnswer */) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+  return Promise.all(array);
 }
 
 /**
